6a76059f9731ef1100eb11b34f88da59e974fb6e
Sid <siddharthbelbase@gmail.com>
2020-04-24 22:35:07 +0545

    Added Simple Line Icons

--- whocalledme/main/routes.py
+++ whocalledme/main/routes.py
@@ -9,14 +9,14 @@ def search_input():
     return render_template('input.html')
 
 
-@main.route('/search', methods=['GET'])
+@main.route('/result', methods=['GET'])
 def search_result():
     if request.method == 'GET':
-        query= request.args.get('number')
-        if query and len(query) == 10 and query.startswith('9'):
+        query= str(request.args.get('number'))
+        if query and len(query) == 10 and query.startswith('9') :
             stripthatdown = query[:4]
         elif query and query.startswith('0'):
             stripthatdown = query[:3]
         result = Extension.query.whooshee_search(stripthatdown, order_by_relevance=-1).first()
         return render_template('result.html', query=query, result=result)
-    return render_template('input.html', query=query)
+    return render_template('notfound.html', query=query)
